c3c81809e776a2b1089ef80f4fd5762dcc729e05
victorverma <vishashv@gmail.com>
2024-07-19 23:24:51 -0400

    Properly handle errors when downloading

--- code/download_raw_data.sh
+++ code/download_raw_data.sh
@@ -9,9 +9,9 @@ urls=(
   "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes13/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g13_s20130601_e20171214_v1-0-0.nc"
   "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes14/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g14_s20090901_e20200304_v1-0-0.nc"
   "https://www.ncei.noaa.gov/data/goes-space-environment-monitor/access/science/xrs/goes15/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g15_s20100331_e20200304_v1-0-0.nc"
-  "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes16/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g16_s20170207_e20240511_v2-2-0.nc"
+  "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes16/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g16_s20170207_e20240715_v2-2-0.nc"
   "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes17/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g17_s20180601_e20230110_v2-2-0.nc"
-  "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes18/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g18_s20220902_e20240511_v2-2-0.nc"
+  "https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes18/l2/data/xrsf-l2-avg1m_science/sci_xrsf-l2-avg1m_g18_s20220902_e20240715_v2-2-0.nc"
 )
 
 files=(
@@ -31,22 +31,24 @@ files=(
 download_file() {
   local index=$1
   echo "Creating ${files[index]}"
-  curl -o "${files[index]}" "${urls[index]}"
+  curl --fail -o "${files[index]}" "${urls[index]}"
   local err_code=$?
   if [ $err_code -eq 0 ]; then
     if [ -s "${files[index]}" ]; then
       echo "File created successfully"
+      return 0
     else
       echo "File doesn't exist or is empty"
+      return 1
     fi
   else
-    echo "curl failed with error code ${err_code}"
+    return 1
   fi
 }
 
 if [ $# -eq 0 ]; then
   for i in "${!files[@]}"; do
-    download_file $i
+    download_file $i || exit $?
   done
 else
   target="$1"
@@ -58,8 +60,9 @@ else
     fi
   done
   if [ $index -ne -1 ]; then
-    download_file $index
+    download_file $index || exit $?
   else
     echo "Satellite not found"
+    exit 1
   fi
 fi
